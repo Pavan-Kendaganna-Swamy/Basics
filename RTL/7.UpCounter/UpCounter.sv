e5565eca229867f3768e4c43cec612712b82bc20
Pavan-Kendaganna-Swamy <pkendaga@asu.edu>
2023-07-20 20:59:09 -0700

    added up counter with negedge reset pin

---
+++ RTL/7.UpCounter/UpCounter.sv
@@ -0,0 +1,18 @@
+module up_counter#(parameter width = 4)(
+    input logic clk_i, 
+    input logic n_rst_i, 
+    output logic [width-1:0] cnt_o
+);
+always_ff@(posedge clk_i, negedge n_rst_i)
+    begin
+        if(~n_rst_i)
+            begin 
+                cnt_o <= 0;
+            end
+        else
+            begin
+                cnt_o <= cnt_o + 1;
+            end
+    end
+
+endmodule
